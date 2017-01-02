622ac87eece9828696651d14cf0690ec0cef33a4
frintar <frintarsys@gmail.com>
2017-01-02 09:56:50 +0700

    Change landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -74,17 +74,11 @@
             @endif
 
             <div class="content">
+                <div><h3>All-New</h3></div>
                 <div class="title m-b-md">
-                    Laravel
+                    Frintar
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <div><small>Coming Soon!</small></div>
             </div>
         </div>
     </body>
